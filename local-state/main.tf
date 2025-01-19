95eda51444178b1add0a6f5a5f0f42aefd41458a
DragneelAyan <ayandragneelsinha@gmail.com>
2025-01-19 22:15:51 +0530

    spelling

--- local-state/main.tf
+++ local-state/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes = [ "10.0.1.0/24" ]
 }
 
-# Define a network inteface
+# Define a network interface
 resource "azurerm_network_interface" "nic" {
   name = "linux-vm-nic"
   location = azurerm_resource_group.rg.location
